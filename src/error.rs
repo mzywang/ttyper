8cb3020423c76ca155d1866eb28667f36c5a385e
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-03-24 18:53:59 -0400

    Dedicated error enum

---
+++ src/error.rs
@@ -0,0 +1,21 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum TtyperError {
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("Terminal error: {0}")]
+    Terminal(String),
+
+    #[error("Application error: {0}")]
+    Application(String),
+
+    #[error("Configuration error: {0}")]
+    Config(String),
+
+    #[error("Content error: {0}")]
+    Content(String),
+}
+
+pub type Result<T> = std::result::Result<T, TtyperError>;
